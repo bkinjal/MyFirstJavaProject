74b8e4a81feb027cad3dcb4feda6f6d98706828b
bkinjal <kinjalpatel2005@yahoo.com>
2015-06-13 10:04:22 -0500

    Adding first file
    Adding first file

---
+++ CondisionalDataTypes.java
@@ -0,0 +1,27 @@
+package com.digitek.business;
+
+public class CondisionalDataTypes {
+	
+	public static void main(String[] args){
+		ifElseExamples();
+		
+		
+	}
+	public static void ifElseExamples(){
+		int no1 = 33;
+		int no2 = 11;
+		
+		int sum = no1+no2;
+		
+		if (sum<33){
+			System.out.println("sum is less then 33 "+ sum);
+		}else if (sum>55){
+			System.out.println("sum is less then 55");
+		}else if (sum==55){
+			System.out.println("sum is equal to 55");
+		}else {
+			System.out.println("nothing of the above");
+
+}
+	}
+}
